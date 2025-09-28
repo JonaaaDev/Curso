2e85eba83a9d14614c777c359d9b944df9705c9d
JonaaaDev <jonathangimenezdev@gmail.com>
2025-09-28 23:34:08 +0200

    Delete 01_curso.py

--- 01_curso.py
+++
@@ -1,9 +0,0 @@
-print("Hola Jonathan")
-
-#formatos de print
-print("hacer salto de linea -> \nprueba.")
-print("hacer tab (tabulación) -> \tprueba.")
-
-#cancelar formato
-print("hacer salto de linea -> \\nprueba.")
-print("hacer tab (tabulación) -> \\tprueba.")
