3982ee076dc5953bf254badebeadb091a2728176
JonaaaDev <jonathangimenezdev@gmail.com>
2025-09-28 23:32:01 +0200

    Create 01curso.py

---
+++ Python/01curso.py
@@ -0,0 +1,9 @@
+print("Hola Jonathan")
+
+#formatos de print
+print("hacer salto de linea -> \nprueba.")
+print("hacer tab (tabulación) -> \tprueba.")
+
+#cancelar formato
+print("hacer salto de linea -> \\nprueba.")
+print("hacer tab (tabulación) -> \\tprueba.")
