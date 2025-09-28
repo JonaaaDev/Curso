3bf22255ebc8a2ad62c1569c237d7face12546bf
JonaaaDev <jonathangimenezdev@gmail.com>
2025-09-28 23:41:21 +0200

    Update 01curso.py

--- Python/01curso.py
+++ Python/01curso.py
@@ -8,3 +8,7 @@ print("hacer tab (tabulación) -> \tprueba.")
 #cancelar formato
 print("hacer salto de linea -> \\nprueba.")
 print("hacer tab (tabulación) -> \\tprueba.")
+
+#varias líneas
+print("""un texto de varias líneas 
+usando tres comillas""")
