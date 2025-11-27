88409d21bd3c06115d414cdf1813c5a41a964295
JonaaaDev <jonathangimenezdev@gmail.com>
2025-11-27 17:39:47 +0100

    feat: vincular player.py en main.py e integrar el jugador

--- pygame/main.py
+++ pygame/main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 pygame.init()
 
@@ -13,13 +14,21 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+player = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    all_sprites.update()
+
     screen.fill(BLACK)
+
+    all_sprites.draw(screen)
 
     pygame.display.flip()
 
